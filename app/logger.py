c4fe4fd2e1cfe6a4ca81ed120a5f2d505571a248
AllexFrolov <mosbka.37@gmail.com>
2024-05-02 13:27:23 +0300

    created global logger

---
+++ app/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+def get_logger():
+    logger = logging.getLogger("stinky_socks_core")
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    if not logger.handlers:
+        logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
