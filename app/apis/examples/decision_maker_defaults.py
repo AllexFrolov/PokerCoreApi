2c8460592480dc36f0e93634d8299d08c8d8b4a3
AllexFrolov <mosbka.37@gmail.com>
2024-05-14 21:47:42 +0300

    added default values

---
+++ app/apis/examples/decision_maker_defaults.py
@@ -0,0 +1,37 @@
+example = {
+    'hand': ['7d', '2h'],
+    'board': [],
+    'pot': 1.5,
+    'stage': 'PREFLOP',
+    'positions': ['OPP_1', 'OPP_2', 'HERO'],
+    'action_sequence': ['HERO', 'OPP_1', 'OPP_2'],
+    'players_stats': {
+        'HERO': {
+            'stack': 30,
+            'bet': 0.,
+            'call_size': 1.,
+            'active': True,
+            'dealer': True,
+            'all_in': False,
+            'sit_out': False
+        },
+        'OPP_1': {
+            'stack': 29.5,
+            'bet': 0.5,
+            'call_size': 0,
+            'active': True,
+            'dealer': True,
+            'all_in': False,
+            'sit_out': False
+        }, 
+        'OPP_2': {
+            'stack': 29,
+            'bet': 1.,
+            'call_size': 0,
+            'active': True,
+            'dealer': False,
+            'all_in': False,
+            'sit_out': False
+        }
+    }
+}
